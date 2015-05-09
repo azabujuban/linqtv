065042608a4b3bd4dfcd57accbfd9dd09aded845
Oleg Levy <oleg.levy@live.com>
2015-05-09 18:50:12 +0900

    added ZIP httpclient - really took waaaaaay too long

---
+++ linqtv/Network/ZipHttpClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Threading;
+using System.Collections.Immutable;
+using System.IO;
+using System.IO.Compression;
+
+namespace linqtv.Network
+{
+    public class ZipHttpClient
+    {
+        private HttpClient _httpClient = new HttpClient();
+
+        public async Task<IImmutableDictionary<string, Stream>> GetAsync(string uri, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var response = await _httpClient.GetAsync(uri, cancellationToken);
+            response.EnsureSuccessStatusCode();
+
+            if (!"application/zip".Equals(response.Content.Headers.ContentType.MediaType, StringComparison.OrdinalIgnoreCase))
+                //TODO: need better exception types
+                throw new Exception($"Expected application/zip but got {response.Content.Headers.ContentType.MediaType}");
+
+            var zipArchive = new ZipArchive(await response.Content.ReadAsStreamAsync());
+            return zipArchive.Entries.Select(e => new KeyValuePair<string, Stream>(e.FullName, e.Open())).ToImmutableDictionary();
+        }
+
+    }
+}
---
+++ tests/NetworkTests.cs
@@ -0,0 +1,33 @@
+﻿using linqtv.Network;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace tests
+{
+    [TestClass]
+    public class NetworkTests
+    {
+        [TestMethod]
+        public async Task Network1()
+        {
+            var uri = "http://thetvdb.com/api/17D761404C40D3C4/series/70336/all/en.zip";
+            var zipClient = new ZipHttpClient();
+
+            var someData = zipClient.GetAsync(uri);
+
+            try
+            {
+                var d = await someData;
+
+                Assert.AreEqual(d.Count, 3);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
