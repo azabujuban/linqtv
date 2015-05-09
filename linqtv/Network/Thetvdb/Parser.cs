061adc8753bd670cc08a53686e639722b462e340
Oleg Levy <oleg.levy@live.com>
2015-05-10 01:32:09 +0900

    added streaming xml parsing

---
+++ linqtv/Network/Thetvdb/Parser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using linqtv.Model;
+
+namespace linqtv.Network.Thetvdb
+{
+    public class Parser
+    {
+        private readonly Stream _xmlStream;
+
+        private readonly List<Show> _parsedShows = new List<Show>();
+        private readonly List<Episode> _parsedEpisodes = new List<Episode>();
+        private readonly static HashSet<string> _interestingElements = new HashSet<string> { _episodeConstant, _seriesConstant };
+
+        private const string _episodeConstant = "episode";
+        private const string _seriesConstant = "series";
+
+
+        public Parser(Stream xmlStream)
+        {
+            _xmlStream = xmlStream;
+        }
+
+        private void ParseXmlStream()
+        {
+            foreach (var e in Xml.StreamingAxis.AsEnumerable(_xmlStream, _interestingElements))
+            {
+                if (_episodeConstant.Equals(e.Name.LocalName, StringComparison.OrdinalIgnoreCase))
+                    _parsedEpisodes.Add(Episode.FromXElement(e));
+
+                if (_seriesConstant.Equals(e.Name.LocalName, StringComparison.OrdinalIgnoreCase))
+                    _parsedShows.Add(Show.FromXElement(e));
+            }
+
+
+        }
+
+        public IEnumerable<Show> Shows => _parsedShows;
+        public IEnumerable<Episode> Episodes => _parsedEpisodes;
+    }
+}
---
+++ linqtv/Xml/StreamingAxis.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+
+namespace linqtv.Xml
+{
+    public class StreamingAxis
+    {
+        public static IEnumerable<XElement> AsEnumerable(Stream xmlStream, ISet<string> elementNames)
+        {
+            var normalizedNames = elementNames.Select(n => n.ToLower());
+
+            using (var xmlReader = XmlReader.Create(xmlStream))
+            {
+                xmlReader.MoveToContent();
+
+                while (xmlReader.Read())
+                {
+                    if ((XmlNodeType.Element == xmlReader.NodeType) && normalizedNames.Contains(xmlReader.Name))
+                    {
+                        var yieldElement = XNode.ReadFrom(xmlReader) as XElement;
+                        if (null != yieldElement)
+                            yield return yieldElement;
+                    }
+                }
+                xmlReader.Close();
+            }
+        }
+    }
+}
