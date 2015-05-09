373cf06ed2de80360370b340981e3a00cbb4cba7
Oleg Levy <oleg.levy@live.com>
2015-05-09 13:13:56 +0900

    added Model.Show

--- linqtv/Model/show.cs
+++ linqtv/Model/show.cs
@@ -1,10 +1,42 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 
 namespace linqtv.Model
 {
+    public enum StatusEnum
+    {
+        Ended,
+        Continuing
+    }
+
     public class Show
     {
-        public IEnumerable<Episode> Episodes {get;}
-        public string Name { get; }
+        public uint id { get; private set; }
+        public IImmutableList<string> Actors { get; private set; }
+        public string Airs_DayOfWeek { get; private set; }
+        public DateTime Airs_Time { get; private set; }
+        public string ContentRating { get; private set; }
+        public DateTime FirstAired { get; private set; }
+        public IImmutableList<string> Genre { get; private set; }
+        public string IMDB_ID { get; private set; }
+        public string Language { get; private set; }
+        public string Network { get; private set; }
+        public uint? NetworkID { get; private set; }
+        public string Overview { get; private set; }
+        public float? Rating { get; private set; }
+        public uint? RatingCount { get; private set; }
+        public uint? Runtime { get; private set; }
+        public string SeriesName { get; private set; }
+        public StatusEnum? Status { get; private set; }
+        public DateTime added { get; private set; }
+        public uint? addedBy { get; private set; }
+        public string banner { get; private set; }
+        public string fanart { get; private set; }
+        public DateTime lastupdated { get; private set; }
+        public IImmutableList<string> posters { get; private set; }
+        public string zap2it_id { get; private set; }
+
+        public IEnumerable<Episode> Episodes {get; private set; }
     }
 }
