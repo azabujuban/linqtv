0f57365ea25a579acc232c78771de6b79fee1265
Oleg Levy <oleg.levy@live.com>
2015-05-20 12:01:52 +0900

    Revert "we dont really need this context"
    This reverts commit 11dc5d109f1c5e40fd0bc13fa5a1439db8a604af.

---
+++ linqtv/IContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+using linqtv.Model;
+
+namespace linqtv
+{
+    public interface IContext : IDisposable
+    {
+        IQueryable<Show> Shows { get; }
+    }
+}
