4a612970dcefd817c0b97d1bc206c696d18d25b8
Oleg Levy <oleg.levy@live.com>
2015-05-20 12:26:01 +0900

    updated to the latest relinq version

--- linqtv/Linq/TvdbQueryExpressionVisitor.cs
+++ linqtv/Linq/TvdbQueryExpressionVisitor.cs
@@ -6,14 +6,14 @@ using System.Collections.Immutable;
 
 namespace linqtv.Linq
 {
-    public class TvdbQueryExpressionVisitor : ThrowingExpressionTreeVisitor
+    public class TvdbQueryExpressionVisitor : ThrowingExpressionVisitor
     {
         private ImmutableDictionary<string, string> AccumulatedParams { get; set; } = ImmutableDictionary<string, string>.Empty;
 
         public static IDictionary<string, string> GenerateUrlParams(Expression expression)
         {
             var visitor = new TvdbQueryExpressionVisitor();
-            visitor.VisitExpression(expression);
+            visitor.Visit(expression);
             return visitor.AccumulatedParams;
         }
 
@@ -22,7 +22,7 @@ namespace linqtv.Linq
             throw new NotImplementedException($"{visitMethod} visit method is not implemented in {nameof(TvdbQueryExpressionVisitor)}");
         }
 
-        protected override Expression VisitMemberExpression(MemberExpression expression)
+        protected override Expression VisitMember(MemberExpression expression)
         {
             if ((expression.Member.Name != "SeriesName") || (expression.Member.DeclaringType != typeof(Model.Show)))
                 throw new NotImplementedException("Only .Name property of Show is supported");
@@ -30,9 +30,9 @@ namespace linqtv.Linq
             return expression;
         }
 
-        protected override Expression VisitConstantExpression(ConstantExpression expression) => expression;
+        protected override Expression VisitConstant(ConstantExpression expression) => expression;
 
-        protected override Expression VisitBinaryExpression(BinaryExpression expression)
+        protected override Expression VisitBinary(BinaryExpression expression)
         {
             if (expression.NodeType != ExpressionType.Equal)
                 throw new NotSupportedException("Only equal binary expressions supported");
@@ -43,8 +43,8 @@ namespace linqtv.Linq
             if (expression.Right.NodeType != ExpressionType.Constant)
                 throw new NotSupportedException($"Right side of a binary expression in .Where must be a {nameof(ExpressionType.Constant)}");
 
-            var leftMember = VisitExpression(expression.Left) as MemberExpression;
-            var rightConstant = VisitExpression(expression.Right) as ConstantExpression;
+            var leftMember = Visit(expression.Left) as MemberExpression;
+            var rightConstant = Visit(expression.Right) as ConstantExpression;
 
             AccumulatedParams = AccumulatedParams.SetItem(leftMember.Member.Name, rightConstant.Value as string);
 
