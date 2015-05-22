a9e9102907ab12b3a40e5c38a3a8095fca0e34d0
Oleg Levy <oleg.levy@live.com>
2015-05-22 11:19:08 +0900

    added the cli app

---
+++ cli/Commands.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CLAP;
+using Linqtv;
+using Linqtv.Linq;
+using Linqtv.Model;
+
+namespace Cli
+{
+    public class Commands
+    {
+        [Verb]
+        void GetShow(string apikey, string name, bool episodes, int verbosity)
+        {
+            using (var showContext = TvdbQueryable<Show>.Create(apikey))
+            {
+                var shows = from show in showContext
+                    where show.SeriesName == name
+                    select show;
+
+                foreach (var s in shows)
+                {
+                    var firstAired = s.FirstAired?.Date.ToString(CultureInfo.InvariantCulture) ?? string.Empty;
+                    var network = s?.Network ?? string.Empty;
+
+                    Console.WriteLine($"{s.SeriesName} on {network} ({firstAired}-{s.Status})");
+                }
+            }
+        }
+    }
+}
---
+++ cli/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CLAP;
+
+namespace Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Parser<Commands>().Run(args, new Commands());
+        }
+    }
+}
